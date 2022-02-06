7dcabc18a5b779adf974c9a19c1c8f8af0a68634
Cameron Rosenberger <crosenberger17@gmail.com>
2022-02-06 11:15:09 -0800

    added different variable types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import React from "react";
-
 import "./App.css";
+
+// use any if you don't want to specify a type, not recommended
+let name: any;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// using void will return undefined, whereas never won't return anything
+let printName: (name: string) => never;
+
+/* type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person;
+
+let lotsOfPeople: Person[];
+ */
+
+// use uknown type instead of any when type is otherwise not specified
+let personName: unknown;
 
 function App() {
   return <div className="App">Hello World</div>;
