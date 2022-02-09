043bea5dfe61238253aeb37019b5b9b63551e9a3
Cameron Rosenberger <crosenberger17@gmail.com>
2022-02-09 10:40:15 -0800

    added styles to task project

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-// use any if you don't want to specify a type, not recommended
-let name: any;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-// using void will return undefined, whereas never won't return anything
-let printName: (name: string) => never;
-
-/* type Person = {
-  name: string;
-  age?: number;
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField />
+    </div>
+  );
 };
-
-let person: Person;
-
-let lotsOfPeople: Person[];
- */
-
-// use uknown type instead of any when type is otherwise not specified
-let personName: unknown;
-
-function App() {
-  return <div className="App">Hello World</div>;
-}
 
 export default App;
